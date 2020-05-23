304022c69d3c375fd4537346da568c5b3bb382d6
Jeremiah Darais <mail@jeremiahdarais.com>
2020-05-22 19:01:10 -0700

    Add built artifacts to the repository, so they can be used without needing to build

---
+++ lib/index.d.ts
@@ -0,0 +1,39 @@
+declare type PageData = any;
+declare type SiteData = any;
+export interface Page {
+    data: PageData;
+    render: (data: PageData) => Promise<string>;
+}
+export interface CreatePagesFromFilesParams {
+    rootDir?: string;
+    filePattern: string;
+    fileToData: (fileContents: string) => Promise<PageData>;
+    render: (data: PageData) => Promise<string>;
+    getLink: (data: PageData) => string;
+    siteData?: SiteData;
+}
+export declare function createPagesFromFiles(params: CreatePagesFromFilesParams): Promise<Page[]>;
+export interface CreatePageParams {
+    data: PageData;
+    render: (data: PageData) => Promise<string>;
+    link: string;
+    siteData?: SiteData;
+}
+export declare function createPage(params: CreatePageParams): Page;
+export declare function addSequenceLinks(pages: Page[]): void;
+export declare class SiteGenerator {
+    private pages;
+    private assets;
+    private outDir;
+    constructor(outDir: string);
+    addPages(pages: Page): void;
+    addAssets(sourceDir: string, pattern: string): void;
+    generate(): Promise<void>;
+}
+export interface Asset {
+    basePath: string;
+    path: string;
+}
+export declare function copyAssets(assets: Asset[], outDir: string): Promise<void>;
+export declare function generatePages(pages: Page[], outDir: string): Promise<void>;
+export {};
